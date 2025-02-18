a337f5ad951a535128c0137f2056fd1c7b43f87f
Pedro <secundary9618@gmail.com>
2025-02-18 23:16:06 +0100

    sql2

--- RE2.sql
+++ RE2.sql
@@ -39,7 +39,52 @@ END //
 
 DELIMITER ;
 
-#3  Crea un procedimiento que muestre cuantos pedidos se han hecho en una fecha que recibe como 
-parámetro. Si no existe ninguno debe mostrar “No hay pedidos en esa fecha”.
+#3  Dice si hay o no pedidos en una fecha que damos como parametro.
 
+DELIMITER //
 
+CREATE PROCEDURE cuentaPedidos(IN fecha_param DATE)
+BEGIN
+    DECLARE contador INT;
+    DECLARE mensaje VARCHAR(255);
+    
+    SELECT COUNT(*) INTO contador
+    FROM pedidos
+    WHERE fechaPedido = fecha_param;
+    
+    IF contador > 0 THEN
+        SET mensaje = 'Hay pedidos en esa fecha';
+    ELSE
+        SET mensaje = 'No hay en esa fecha';
+    END IF;
+    
+    SELECT mensaje AS resultado;
+END //
+
+DELIMITER ;
+
+#4  Crea un procedimiento que recibe como parámetro un año y cuenta los pedidos de ese año.  
+Después mostrará devolverá en un parámetro de salida los siguientes mensajes según corresponda:
+ “Menos de 100 pedidos”
+ “Entre 100 y 150 pedidos”
+ “Más de 150 pedidos”
+ 
+DELIMITER //
+ 
+CREATE PROCEDURE cuentaPedidosAnual(IN ano_param DATE)
+BEGIN
+    DECLARE contador INT;
+    DECLARE mensaje VARCHAR(255);
+    
+    SELECT COUNT(*) INTO contador
+    FROM pedidos
+    WHERE fechaPedido = ano_param;
+    
+    IF contador > 100 THEN
+        SET mensaje = 'Hay pedidos';
+    ELSE
+        SET mensaje = 'No hay en esa fecha';
+    END IF;
+    
+    SELECT mensaje AS resultado;
+END //
