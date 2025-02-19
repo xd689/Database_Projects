4b79db416724abc9c005cc6d3fa778589f9a3600
Pedro <secundary9618@gmail.com>
2025-02-19 01:19:50 +0100

    proceduresfinal

--- RE2.sql
+++ RE2.sql
@@ -64,10 +64,10 @@ END //
 DELIMITER ;
 
 #4  Crea un procedimiento que recibe como parámetro un año y cuenta los pedidos de ese año.  
-Después mostrará devolverá en un parámetro de salida los siguientes mensajes según corresponda:
- “Menos de 100 pedidos”
- “Entre 100 y 150 pedidos”
- “Más de 150 pedidos”
+# Después mostrará devolverá en un parámetro de salida los siguientes mensajes según corresponda:
+# “Menos de 100 pedidos”
+# “Entre 100 y 150 pedidos”
+# “Más de 150 pedidos”
  
 DELIMITER //
  
@@ -81,10 +81,37 @@ BEGIN
     WHERE fechaPedido = ano_param;
     
     IF contador > 100 THEN
-        SET mensaje = 'Hay pedidos';
+        SET mensaje = 'Menos de 100 pedidos';
+    ELSEIF contador < 150 THEN
+        SET mensaje = 'Mas de 150 pedidos';
     ELSE
-        SET mensaje = 'No hay en esa fecha';
+        SET mensaje = 'Entre 100 y 150 pedidos';
     END IF;
     
     SELECT mensaje AS resultado;
 END //
+
+DELIMITER ;
+#5 Crea un procedimiento que recibe el nombre de una categoría y un número.
+# El procedimiento mostrará tantos productos de la categoría como indique el número.
+# Por ejemplo si recibe la categoría “pulseras” y el número 5, mostrará cinco productos de la categoría “pulseras”.
+# El número debe estar entre 1 y 10, de lo contrario no hará nada y mostrará el mensaje “El número no es válido”.
+
+DELIMITER //
+
+CREATE PROCEDURE verCategoria(IN categorias VARCHAR(255), IN numero INT)
+BEGIN
+    DECLARE mensaje VARCHAR(255);
+
+    IF numero <= 10 THEN
+        SELECT codProducto, nombre, categoria
+        FROM productos 
+        WHERE categoria = categorias
+        LIMIT numero;
+    ELSE
+        SET mensaje = 'El número no es válido';
+        SELECT mensaje AS resultado;
+    END IF;
+END //
+
+DELIMITER ;
